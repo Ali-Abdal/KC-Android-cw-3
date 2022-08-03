412b973f8421f993312489c0538c20fa9e4ffa04
Ali-Abdal <109912475+Ali-Abdal@users.noreply.github.com>
2022-08-04 00:09:03 +0300

    Cw3

---
+++ Cw3/app/src/main/java/com/example/cw3/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.cw3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button z = findViewById(R.id.PressWontBite);
+        TextView T1 = findViewById(R.id.CalcMain);
+        TextView T2 = findViewById(R.id.Total);
+        EditText E1 = findViewById(R.id.FirstNumber);
+        EditText E2 = findViewById(R.id.SecondNumber);
+        TextView True = findViewById(R.id.FactNoCap);
+
+
+    }
+}
